fd701d5959caf9b743d26e7c4b9ec412c998bbc8
Ricardo Presilla <presilla.r@opendevpro.com>
2023-10-27 16:33:24 -0300

    Add main

---
+++ src/main/java/com/alfaCentauri/CamelTestingApplication.java
@@ -0,0 +1,11 @@
+package com.alfaCentauri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelTestingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CamelTestingApplication.class, args);
+    }
+}
